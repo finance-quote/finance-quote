a50644dd5019b6741d8346da7d8cd19e83182051
Paul Fenwick <poppen.jp@gmail.com>
2000-04-06 06:37:57 +0000

    Example scripts written and placed in Examples.  This involves a move of the Quote_example.pl file.

---
+++ Examples/chkshares.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use strict;
+use lib '..';
+use Finance::Quote qw/asx/;
+use Data::Dumper;
+
+=head1 NAME
+
+chkshares.pl - Check ASX share information.
+
+=head1 USAGE
+
+chkshares.pl TLS CML ITE
+
+=head1 NOTES
+
+Example program.  Demonstrates how to use one of the interfaces to
+Finance::Quote.
+
+In the future this program will be expanded to handle other Finance::Quote
+interfaces (eg, yahoo and yahoo_europe).
+
+=cut
+
+foreach my $code (@ARGV) {
+	print Dumper(\{asx($code)});
+}
