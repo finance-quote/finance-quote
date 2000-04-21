89cdb1a90fa20e068e266f334cef2d7467e26c56
Paul Fenwick <pjf@perltraining.com.au>
2000-04-21 07:59:22 +0000

    New stockdump script which makes use of the fetch function.  It's a handy debugging tool.

---
+++ Examples/stockdump.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+use strict;
+use Finance::Quote;
+use Data::Dumper;
+
+# A very very simple script.  Takes a source and a symbol, looks it up,
+# and dumps it to STDOUT.  Useful for debugging.
+
+die "Usage: $0 source symbol\n" unless (defined $ARGV[1]);
+
+my $q = Finance::Quote->new;
+
+my %quotes = $q->fetch(@ARGV);
+
+print Dumper(\%quotes);
+
