6ab2b6612620646e2758aa2c09c5814d85d7d40d
Paul Fenwick <pjf@perltraining.com.au>
2000-06-23 12:36:01 +0000

    Updated to allow currency to be specified.

--- Examples/stockdump.pl
+++ Examples/stockdump.pl
@@ -3,13 +3,22 @@ use strict;
 use lib '../lib';
 use Finance::Quote;
 use Data::Dumper;
+use Getopt::Std;
 
 # A very very simple script.  Takes a source and a symbol, looks it up,
 # and dumps it to STDOUT.  Useful for debugging.
 
-die "Usage: $0 source symbol\n" unless (defined $ARGV[1]);
+my %options = ('c' => '');
+
+getopts('c:',\%options);
+
+die "Usage: $0 [-c currency] source symbol\n" unless (defined $ARGV[1]);
 
 my $q = Finance::Quote->new;
+
+if ($options{'c'}) {
+	$q->set_currency($options{'c'});
+}
 
 my %quotes = $q->fetch(@ARGV);
 
