696865cefc73a8528a153b6be3ce841b86ec5745
David Hampton <hampton-sf@rainbolthampton.net>
2006-04-06 05:53:15 +0000

    New module from Dominique Corbex <domcox@sourceforge.net> to fetch information from the "Paris Stock Exchange", http://www.boursorama.com.

--- lib/Finance/Quote.pm
+++ lib/Finance/Quote.pm
@@ -166,7 +166,7 @@ sub new {
 	if (!@reqmodules or $reqmodules[0] eq "-defaults") {
 		shift(@reqmodules) if (@reqmodules);
 		# Default modules
-		@modules = qw/AEX ASEGR ASX BMONesbittBurns Cdnfundlibrary Deka
+		@modules = qw/AEX ASEGR ASX BMONesbittBurns Bourso Cdnfundlibrary Deka
 			      DWS FTPortfolios Fidelity FinanceCanada Fool IndiaMutual
 			      ManInvestments NZX Platinum SEB TSP Tdefunds
 			      Tdwaterhouse Tiaacref Troweprice Trustnet Union
