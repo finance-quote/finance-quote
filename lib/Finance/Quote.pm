e42978eb111fa7d5276da58121c50c69f9abdc31
Erik Colson <eco@ecocode.net>
2008-10-11 17:41:02 +0200

    isoTime function added

--- lib/Finance/Quote.pm
+++ lib/Finance/Quote.pm
@@ -664,6 +664,21 @@ sub store_date
     $inforef->{$symbol, "isodate"} = sprintf "%04d-%02d-%02d", $year, $month, $day;
 }
 
+sub isoTime {
+  my ($self,$timeString) = @_ ;
+  $timeString =~ tr/ //d ;
+  my $retTime = "00:00"; # return zero time if unparsable input
+  if ($timeString=~m/^(\d+)[\.:uh](\d+)(AM|PM)?/) {
+    my ($hours,$mins)= ($1,$2) ;
+    $hours+=12 if ($3 && ($3 eq "PM")) ;
+    if ($hours>=0 && $hours<=23 && $mins>=0 && $mins<=59 ) {
+      $retTime = $hours>=10 ? "$hours:" : "0$hours:" ;
+      $retTime.= $mins>=10 ? "$mins" :"0$mins" ;
+    }
+  }
+  return $retTime;
+}
+
 # Dummy destroy function to avoid AUTOLOAD catching it.
 sub DESTROY { return; }
 
@@ -896,6 +911,13 @@ be scaled but any surrounding text preserved.  It's most useful in writing
 new Finance::Quote modules where you may retrieve information in a
 non-ISO4217 unit (such as cents) and would like to scale it to a more
 useful unit (like dollars).
+
+=head2 ISOTIME
+
+    $q->isoTime("11:39PM");    # returns "23:39"
+    $q->isoTime("9:10 AM");    # returns "09:10"
+
+This function will return a isoformatted time
 
 =head1 ENVIRONMENT
 
