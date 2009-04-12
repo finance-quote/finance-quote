6149119fa0b833b3bbb5d7b9fe4fecd3ef7539e3
Erik Colson <eco@ecocode.net>
2009-04-12 18:34:58 +0200

    Variables renamed and various code cleanup

--- lib/Finance/Quote/Morningstar.pm
+++ lib/Finance/Quote/Morningstar.pm
@@ -1,4 +1,4 @@
-package Finance::Quote::Ftest;
+package Finance::Quote::Morningstar;
 require 5.004;
 
 use strict;
@@ -12,15 +12,15 @@ use HTML::TableExtract;
 $VERSION = '1.0';
 $MORNINGSTAR_SE_FUNDS_URL = 'http://morningstar.se/funds/overview.asp?cid=';
 
-sub methods { return (ftest_funds => \&ftest_funds); }
+sub methods { return (morningstar => \&morningstar); }
 
 {
   my @labels = qw/date isodate method source name currency price/;
-	
-  sub labels { return (fredrik_funds => \@labels); }
+
+  sub labels { return (morningstar => \@labels); }
 }
 
-sub ftest_funds {
+sub morningstar {
   my $quoter  = shift;
   my @symbols = @_;
 
@@ -28,54 +28,55 @@ sub ftest_funds {
   my ($ua, $reply, $url, %funds, $te, $table, $row, @value_currency, $name);
 
   foreach my $symbol (@symbols) {
-      $name = $symbol;
-      $url = $MORNINGSTAR_SE_FUNDS_URL;
-      $url = $url . $name;
-      $ua    = $quoter->user_agent;
-      $reply = $ua->request(GET $url);
-      unless ($reply->is_success) {
+    $name = $symbol;
+    $url = $MORNINGSTAR_SE_FUNDS_URL;
+    $url = $url . $name;
+    $ua    = $quoter->user_agent;
+    $reply = $ua->request(GET $url);
+    unless ($reply->is_success) {
 	  foreach my $symbol (@symbols) {
-	      $funds{$symbol, "success"}  = 0;
-	      $funds{$symbol, "errormsg"} = "HTTP failure";
+        $funds{$symbol, "success"}  = 0;
+        $funds{$symbol, "errormsg"} = "HTTP failure";
 	  }
 	  return wantarray ? %funds : \%funds;
-      }
+    }
 
-      $te = new HTML::TableExtract();
-      $te->parse($reply->content);
-      #print "Tables: " . $te->tables_report() . "\n";
-      my $counter = 0;
-      my $dateset = 0;
-      for my $table ($te->tables()) {
+    $te = new HTML::TableExtract();
+    $te->parse($reply->content);
+    #print "Tables: " . $te->tables_report() . "\n";
+    my $counter = 0;
+    my $dateset = 0;
+    for my $table ($te->tables()) {
 	  for my $row ($table->rows()) {
-	      if (defined(@$row[0])) {
+        if (defined(@$row[0])) {
 		  if ('Senaste NAV' eq substr(@$row[0],0,11)) {
-		      @value_currency = split(/ /, $$row[2]);
-		      $funds{$name, 'method'}   = 'fredrik_funds';
-		      $funds{$name, 'price'}    = $value_currency[0];
-		      $funds{$name, 'currency'} = $value_currency[1];
-		      $funds{$name, 'success'}  = 1;
-		      $funds{$name, 'symbol'}  = $name;
-		      $funds{$name, 'source'}   = 'Finance::Quote::Ftest';
-		      $funds{$name, 'name'}   = $name;
+            @value_currency = split(/ /, $$row[2]);
+            $funds{$name, 'method'}   = 'morningstar_funds';
+            $funds{$name, 'price'}    = $value_currency[0];
+            $funds{$name, 'currency'} = $value_currency[1];
+            $funds{$name, 'success'}  = 1;
+            $funds{$name, 'symbol'}  = $name;
+            $funds{$name, 'source'}   = 'Finance::Quote::Morningstar';
+            $funds{$name, 'name'}   = $name;
+            $funds{$name, 'p_change'} = "";  # p_change is not retrieved (yet?)
 		  }
 		  if ($counter == 7 && $dateset == 0) {
-		      my $date = substr($$row[1],0,10);
-		      $quoter->store_date(\%funds, $name, {isodate => $date});
-		      $dateset = 1;
+            my $date = substr($$row[1],0,10);
+            $quoter->store_date(\%funds, $name, {isodate => $date});
+            $dateset = 1;
 		  }
-	      }
+        }
 	  }
 	  $counter++;
-      }
+    }
 
-      # Check for undefined symbols
-      foreach my $symbol (@symbols) {
+    # Check for undefined symbols
+    foreach my $symbol (@symbols) {
 	  unless ($funds{$symbol, 'success'}) {
-	      $funds{$symbol, "success"}  = 0;
-	      $funds{$symbol, "errormsg"} = "Fund name not found";
+        $funds{$symbol, "success"}  = 0;
+        $funds{$symbol, "errormsg"} = "Fund name not found";
 	  }
-      }
+    }
   }
   return %funds if wantarray;
   return \%funds;
@@ -85,7 +86,7 @@ sub ftest_funds {
 
 =head1 NAME
 
-Finance::Quote::Ftest - Obtain fund prices the Fredrik way
+Finance::Quote::Morningstar - Obtain fund prices the Fredrik way
 
 =head1 SYNOPSIS
 
@@ -93,12 +94,12 @@ Finance::Quote::Ftest - Obtain fund prices the Fredrik way
 
     $q = Finance::Quote->new;
 
-    %fundinfo = $q->fetch("fredrik_funds","fund name");
+    %fundinfo = $q->fetch("morningstar","fund name");
 
 =head1 DESCRIPTION
 
 This module obtains information about Fredrik fund prices from
-www.morningstar.se.  
+www.morningstar.se.
 
 =head1 FUND NAMES
 
