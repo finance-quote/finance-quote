1ce073c2a924697f09180bab593b8b5d06a54551
Erik Colson <eco@ecocode.net>
2010-02-13 09:21:31 +0100

    Patch 1 dd 2010-02-11 from Simon Lindgren applied to Morningstar

--- lib/Finance/Quote/Morningstar.pm
+++ lib/Finance/Quote/Morningstar.pm
@@ -3,14 +3,21 @@ require 5.004;
 
 use strict;
 
-use vars qw($VERSION $MORNINGSTAR_SE_FUNDS_URL);
+use vars qw($VERSION $MORNINGSTAR_SE_FUNDS_URL $PRICE_PATH $NAME_PATH $DATE_PATH $PCHANGE_PATH);
 
+use HTML::TreeBuilder::XPath;
+use Encode;
 use LWP::UserAgent;
 use HTTP::Request::Common;
-use HTML::TableExtract;
 
 $VERSION = '1.17';
-$MORNINGSTAR_SE_FUNDS_URL = 'http://morningstar.se/funds/overview.asp?cid=';
+$MORNINGSTAR_SE_FUNDS_URL = 
+    'http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=';
+$PRICE_PATH = '//div/div/span[@title=\'Senaste NAV\']/span';
+$NAME_PATH = 'id(\'content_main\')/div/div/div/div/div/div/div/h2';
+$DATE_PATH = '//div/div/span[@title=\'NAV-datum\']/span';
+$PCHANGE_PATH = 
+    decode_utf8 '//div/div/span[@title=\'Ändring NAV en dag (SEK)\']/span';
 
 sub methods { return (morningstar => \&morningstar); }
 
@@ -20,63 +27,123 @@ sub methods { return (morningstar => \&morningstar); }
   sub labels { return (morningstar => \@labels); }
 }
 
+sub writeLog
+{
+  #Uncomment for some debug info
+  #my $msg = shift;
+  #open LOGFILE, ">>$ENV{HOME}/morningstar.log";
+  #print LOGFILE $msg . "\n";
+  #close LOGFILE or print LOGFILE "Error closing log file";
+}
+
+sub failDocument
+{
+  my ($funds, $symbol, $reason) = @_;
+  $$funds{$symbol, "success"}  = 0;
+  $$funds{$symbol, "errormsg"} =
+      "Invalid document, could not find the " . $reason . ".";
+  writeLog $$funds{$symbol, "errormsg"};
+}
+
+#HACK: This is a terrible hack, but it speeds up the parsing by ~25%
+#this WILL damage the markup, so lets hope HTML::TreeBuilder
+#is good at it's job.
+sub filter
+{
+  my $src = shift;
+  $src =~ s/<(script|br|input|li|ul|select|tr|a|td|option|table|fieldset)[^>]*>.*?<\/\1>//gs;
+  #writeLog $src;
+  return $src;
+}
+
 sub morningstar {
   my $quoter  = shift;
   my @symbols = @_;
 
   return unless @symbols;
-  my ($ua, $reply, $url, %funds, $te, $table, $row, @value_currency, $name);
+  my ($ua, $reply, $url, %funds, $tree, $data, $raw_nav, $nav, $raw_date, $date, $raw_name, $name, $raw_p_change, $p_change, $navCurrency);
 
   foreach my $symbol (@symbols) {
-    $name = $symbol;
-    $url = $MORNINGSTAR_SE_FUNDS_URL;
-    $url = $url . $name;
+    writeLog "\nFetching symbol: " . $symbol;
+    $url = $MORNINGSTAR_SE_FUNDS_URL . $symbol;
     $ua    = $quoter->user_agent;
     $reply = $ua->request(GET $url);
     unless ($reply->is_success) {
-	  foreach my $symbol (@symbols) {
-        $funds{$symbol, "success"}  = 0;
-        $funds{$symbol, "errormsg"} = "HTTP failure";
-	  }
-	  return wantarray ? %funds : \%funds;
+      $funds{$symbol, "success"}  = 0;
+      $funds{$symbol, "errormsg"} = "HTTP failure: " . $reply->status_line;
+      writeLog $funds{$symbol, "errormsg"};
+      return wantarray ? %funds : \%funds;
     }
 
-    $te = new HTML::TableExtract();
-    $te->parse($reply->content);
-    #print "Tables: " . $te->tables_report() . "\n";
-    my $counter = 0;
-    my $dateset = 0;
-    for my $table ($te->tables()) {
-	  for my $row ($table->rows()) {
-        if (defined(@$row[0])) {
-		  if ('Senaste NAV' eq substr(@$row[0],0,11)) {
-            @value_currency = split(/ /, $$row[2]);
-            $funds{$name, 'method'}   = 'morningstar_funds';
-            $funds{$name, 'price'}    = $value_currency[0];
-            $funds{$name, 'currency'} = $value_currency[1];
-            $funds{$name, 'success'}  = 1;
-            $funds{$name, 'symbol'}  = $name;
-            $funds{$name, 'source'}   = 'Finance::Quote::Morningstar';
-            $funds{$name, 'name'}   = $name;
-            $funds{$name, 'p_change'} = "";  # p_change is not retrieved (yet?)
-		  }
-		  if ($counter == 7 && $dateset == 0) {
-            my $date = substr($$row[1],0,10);
-            $quoter->store_date(\%funds, $name, {isodate => $date});
-            $dateset = 1;
-		  }
-        }
-	  }
-	  $counter++;
+    $tree = HTML::TreeBuilder::XPath->new;
+    $data = filter decode_utf8($reply->content);
+    $tree->parse_content($data);
+    
+    #Verify the existence of the data that we want
+
+    #Check for the price
+    $raw_nav = $tree->findvalue($PRICE_PATH);
+    #unless ($tree->exists($PRICE_PATH)) {
+    if ($raw_nav eq '') {
+      failDocument \%funds, $symbol, "price";
+      return wantarray ? %funds : \%funds;
     }
 
-    # Check for undefined symbols
-    foreach my $symbol (@symbols) {
-	  unless ($funds{$symbol, 'success'}) {
-        $funds{$symbol, "success"}  = 0;
-        $funds{$symbol, "errormsg"} = "Fund name not found";
-	  }
+    #Check for the name of the symbol
+    $raw_name = $tree->findvalue($NAME_PATH); 
+    #unless ($tree->exists($NAME_PATH)) {
+    if ($raw_name eq '') {
+      failDocument \%funds, $symbol, "name";
+      return wantarray ? %funds : \%funds;
     }
+
+    #Check for the date
+    $raw_date = $tree->findvalue($DATE_PATH);
+    #unless ($tree->exists($DATE_PATH)) {
+    if ($raw_date eq '') {
+      failDocument \%funds, $symbol, "date";
+      return wantarray ? %funds : \%funds;
+    }
+
+    #Check for the % change
+    $raw_p_change = $tree->findvalue($PCHANGE_PATH);
+    #unless ($tree->exists($PCHANGE_PATH)) {
+    if ($raw_p_change eq '') {
+      failDocument \%funds, $symbol, "procentual change";
+      return wantarray ? %funds : \%funds;
+    }
+
+    $name = $raw_name;
+
+    $p_change = $raw_p_change;
+    $p_change =~ s/ //;
+    $p_change =~ s/%//;
+    $p_change =~ tr/,/./;
+    
+    #Example contents (between ||): | 1 234,56 SEK|
+    $nav = $raw_nav;
+    $nav =~ /\s+(.*)\s+(\S+)/;
+    $nav = $1;
+    $navCurrency = $2;
+    $nav =~ s/ //; #Remove spaces from inside the number. This is for GNUCash.
+    #Convert between swedish and english number format
+    #123,45 <=> 123.45
+    $nav =~ tr/,/./;
+    
+    writeLog "NAV: |" . $nav . "|\t\tCurrency: " . $navCurrency;
+    $funds{$symbol, 'method'}   = 'morningstar_funds';
+    $funds{$symbol, 'nav'}    = $nav;
+    $funds{$symbol, 'currency'} = $navCurrency;
+    $funds{$symbol, 'success'}  = 1;
+    $funds{$symbol, 'symbol'}  = $symbol;
+    $funds{$symbol, 'source'}   = 'Finance::Quote::Morningstar';
+    $funds{$symbol, 'name'}   = $name;
+    $funds{$symbol, 'p_change'} = $p_change;
+    
+    $date = substr($raw_date,0,10);
+    $quoter->store_date(\%funds, $symbol, {isodate => $date});
+    
+    $tree->delete;
   }
   return %funds if wantarray;
   return \%funds;
@@ -86,7 +153,7 @@ sub morningstar {
 
 =head1 NAME
 
-Finance::Quote::Morningstar - Obtain fund prices the Fredrik way
+Finance::Quote::Morningstar - Fetch fund prices the from the swedish Morningstar
 
 =head1 SYNOPSIS
 
@@ -98,21 +165,37 @@ Finance::Quote::Morningstar - Obtain fund prices the Fredrik way
 
 =head1 DESCRIPTION
 
-This module obtains information about Fredrik fund prices from
-www.morningstar.se.
+This module obtains information about funds from www.morningstar.se.
 
 =head1 FUND NAMES
 
-Use some smart fund name...
+The fund names used are a part of the url to the morningstar site.
+
+For example:
+
+http://morningstar.se/Funds/Quicktake/Overview.aspx?perfid=0P00000RSZ&programid=
+
+Here, the fund name to use is "0P00000RSZ", while this is not exactly a
+friendly name, it is the id that morningstar uses on their website.  
 
 =head1 LABELS RETURNED
 
 Information available from Fredrik funds may include the following labels:
-date method source name currency price. The prices are updated at the
-end of each bank day.
+isodate name currency nav p_change method success
+
+The prices are updated at the end of each bank day.
+
+=head1 COPYRIGHT
+
+  Copyright 2009, Simon Lindgren
+
+=head1 AUTHORS
+
+  Simon Lindgren <simon.n.lindgren@gmail.com>
 
 =head1 SEE ALSO
 
-Perhaps morningstar?
+The Finance::Quote module as well as the morningstar website
+at http://morningstar.se
 
 =cut
--- t/morningstar.t
+++ t/morningstar.t
@@ -15,25 +15,25 @@ my $q      = Finance::Quote->new();
 my $year   = (localtime())[5] + 1900;
 my $lastyear = $year - 1;
 
-my @stocks = ("F0GBR069L2","BOGUS");
+my @stocks = ("0P00000RSZ","BOGUS");
 
 my %quotes = $q->fetch("morningstar",@stocks);
 ok(%quotes);
 
-# Check the nav values are defined.  These are the most
-#  used and most reliable indicators of success.
-ok($quotes{"F0GBR069L2","price"} > 0);
-ok(length($quotes{"F0GBR069L2","name"}) > 0);
-ok($quotes{"F0GBR069L2","success"});
-ok($quotes{"F0GBR069L2", "currency"} eq "USD");
-ok(substr($quotes{"F0GBR069L2","isodate"},0,4) == $year ||
-   substr($quotes{"F0GBR069L2","isodate"},0,4) == $lastyear);
-ok(substr($quotes{"F0GBR069L2","date"},6,4) == $year ||
-   substr($quotes{"F0GBR069L2","date"},6,4) == $lastyear);
+# Check the nav values are defined. These are the most
+# used and most reliable indicators of success.
+ok($quotes{"0P00000RSZ","nav"} > 0);
+ok(length($quotes{"0P00000RSZ","name"}) > 0);
+ok($quotes{"0P00000RSZ","success"});
+ok($quotes{"0P00000RSZ", "currency"} eq "USD");
+ok(substr($quotes{"0P00000RSZ","isodate"},0,4) == $year ||
+   substr($quotes{"0P00000RSZ","isodate"},0,4) == $lastyear);
+ok(substr($quotes{"0P00000RSZ","date"},6,4) == $year ||
+   substr($quotes{"0P00000RSZ","date"},6,4) == $lastyear);
 
 # Make sure we don't have spurious % signs.
 
-ok($quotes{"F0GBR069L2","p_change"} !~ /%/);
+ok($quotes{"0P00000RSZ","p_change"} !~ /%/);
 
 # Check that a bogus stock returns no-success.
 ok(! $quotes{"BOGUS","success"});
