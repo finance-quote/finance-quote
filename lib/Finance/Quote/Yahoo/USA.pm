a4cfca753c45c41fd8d44be0070b7a7aabe8ca79
Erik Colson <eco@ecocode.net>
2014-02-14 10:14:55 +0100

    URL change of Yahoo::USA. Probably correct now.

--- lib/Finance/Quote/Yahoo/USA.pm
+++ lib/Finance/Quote/Yahoo/USA.pm
@@ -41,7 +41,7 @@ $VERSION = '1.18';
 
 # URLs of where to obtain information.
 
-$YAHOO_URL = ("http://finance.yahoo.com/d/hp");
+$YAHOO_URL = ("http://download.finance.yahoo.com/d/quotes.csv");
 
 sub methods {return (canada   => \&yahoo,
                      usa      => \&yahoo,
