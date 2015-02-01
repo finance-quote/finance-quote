5a34d3de6ff54c6d8172513d1311744764cf8f0c
Erik Colson <eco@ecocode.net>
2015-02-01 16:31:26 +0100

    HU - code cleanup and version added

--- lib/Finance/Quote/HU.pm
+++ lib/Finance/Quote/HU.pm
@@ -19,6 +19,8 @@ use HTTP::Request::Common;
 use HTML::TableExtract;
 use Encode;
 
+# VERSION
+
 my $BAMOSZ_MAINURL = "http://www.bamosz.hu/";
 my $BAMOSZ_URL = $BAMOSZ_MAINURL . "alapoldal?isin=";
 
@@ -26,146 +28,161 @@ my $BSE_MAINURL = "http://www.bet.hu/";
 my $BSE_URL = $BSE_MAINURL . "topmenu/kereskedesi_adatok/product_search?isinquery=";
 
 sub methods {
-    return (hufund => \&bamosz, bamosz => \&bamosz, hustock => \&bse,
-	    bse => \&bse, bet => \&bse, hu => \&hu, hungary => \&hu);
+    return ( hufund  => \&bamosz,
+             bamosz  => \&bamosz,
+             hustock => \&bse,
+             bse     => \&bse,
+             bet     => \&bse,
+             hu      => \&hu,
+             hungary => \&hu
+    );
 }
 
 sub labels {
-    my @fundlabels = qw/symbol method source name currency isin date isodate price last/;
-    my @stocklabels = qw/symbol method source currency isin date isodate price open close high low p_change last/;
-    my @alllabels = (@stocklabels, "name");
-    return (hufund => \@fundlabels, bamosz => \@fundlabels,
-	    hustock => \@stocklabels, bse => \@stocklabels,
-	    bet => \@stocklabels, hu => \@alllabels,
-	    hungary => \@alllabels);
+    my @fundlabels =
+        qw/symbol method source name currency isin date isodate price last/;
+    my @stocklabels =
+        qw/symbol method source currency isin date isodate price open close
+           high low p_change last/;
+    my @alllabels = ( @stocklabels, "name" );
+    return ( hufund  => \@fundlabels,
+             bamosz  => \@fundlabels,
+             hustock => \@stocklabels,
+             bse     => \@stocklabels,
+             bet     => \@stocklabels,
+             hu      => \@alllabels,
+             hungary => \@alllabels
+    );
 }
 
 sub hu {
-    my $quoter = shift;
+    my $quoter  = shift;
     my @symbols = @_;
     my %info;
 
     for my $symbol (@symbols) {
-	my %bse_info = bse($quoter, $symbol);
-	if ($bse_info{$symbol, "success"}) {
-	    %info = (%info, %bse_info);
-	    next;
-	}
+        my %bse_info = bse( $quoter, $symbol );
+        if ( $bse_info{ $symbol, "success" } ) {
+            %info = ( %info, %bse_info );
+            next;
+        }
 
-	my %bamosz_info = bamosz($quoter, $symbol);
-	if ($bamosz_info{$symbol, "success"}) {
-	    %info = (%info, %bamosz_info);
-	    next;
-	}
+        my %bamosz_info = bamosz( $quoter, $symbol );
+        if ( $bamosz_info{ $symbol, "success" } ) {
+            %info = ( %info, %bamosz_info );
+            next;
+        }
 
-	$info{$symbol, "success"} = 0;
-	$info{$symbol, "errormsg"} = "Fetch from bse or bamosz failed";
+        $info{ $symbol, "success" }  = 0;
+        $info{ $symbol, "errormsg" } = "Fetch from bse or bamosz failed";
     }
 
     return wantarray() ? %info : \%info;
 }
 
 sub bse {
-    my $quoter = shift;
+    my $quoter  = shift;
     my @symbols = @_;
     my %info;
 
     my $ua = $quoter->user_agent;
 
     for my $symbol (@symbols) {
-	$info{$symbol, "method"} = "bse";
-	$info{$symbol, "source"} = $BSE_MAINURL;
-	$info{$symbol, "success"} = 0;
+        $info{ $symbol, "method" }  = "bse";
+        $info{ $symbol, "source" }  = $BSE_MAINURL;
+        $info{ $symbol, "success" } = 0;
 
-	my $url = $BSE_URL . $symbol;
-	my $response = $ua->request(GET $url);
-	unless ($response->is_success) {
-	    $info{$symbol, "errormsg"} = "Request error";
-	    next;
-	}
+        my $url      = $BSE_URL . $symbol;
+        my $response = $ua->request( GET $url);
+        unless ( $response->is_success ) {
+            $info{ $symbol, "errormsg" } = "Request error";
+            next;
+        }
 
-	my $te = HTML::TableExtract->new(attribs => {class => "InsAdat_table"});
-	$te->parse(decode_utf8($response->content));
-	unless ($te->first_table_found) {
-	    $info{$symbol, "errormsg"} = "No InsAdat_table found";
-	    next;
-	}
+        my $te =
+            HTML::TableExtract->new( attribs => { class => "InsAdat_table" } );
+        $te->parse( decode_utf8( $response->content ) );
+        unless ( $te->first_table_found ) {
+            $info{ $symbol, "errormsg" } = "No InsAdat_table found";
+            next;
+        }
 
-	my @found;
-	TABLE_LOOP: for my $ts ($te->tables) {
-	    for my $row ($ts->rows) {
-		my @trimmed_row = map {trim($_)} @$row;
-		if ($symbol eq $trimmed_row[1] || $symbol eq $trimmed_row[2]) {
-		    @found = @trimmed_row;
-		    last TABLE_LOOP;
-		}
-	    }
-	}
-	unless (@found) {
-	    $info{$symbol, "errormsg"} = "No ticker or ISIN found";
-	    next;
-	}
+        my @found;
+    TABLE_LOOP: for my $ts ( $te->tables ) {
+            for my $row ( $ts->rows ) {
+                my @trimmed_row = map { trim($_) } @$row;
+                if ( $symbol eq $trimmed_row[1] || $symbol eq $trimmed_row[2] )
+                {
+                    @found = @trimmed_row;
+                    last TABLE_LOOP;
+                }
+            }
+        }
+        unless (@found) {
+            $info{ $symbol, "errormsg" } = "No ticker or ISIN found";
+            next;
+        }
 
-	# I don't trade stocks, so I am unsure whether the data
-	# extracted here is sensible. Please do improve this if
-	# needed!
-	$info{$symbol, "symbol"} = $found[1];
-	$info{$symbol, "isin"} = $found[2];
-	$info{$symbol, "price"} = hu_decimal($found[3]);
-	$info{$symbol, "open"} = hu_decimal($found[8]);
-	$info{$symbol, "close"} = hu_decimal($found[9]);
-	$info{$symbol, "currency"} = $found[10];
-	$info{$symbol, "low"} = hu_decimal($found[13]);
-	$info{$symbol, "high"} = hu_decimal($found[14]);
-	$info{$symbol, "p_change"} = hu_decimal($found[16]);
-	$info{$symbol, "last"} = hu_decimal($found[17]);
-	$quoter->store_date(\%info, $symbol, {isodate => $found[18]});
+        # I don't trade stocks, so I am unsure whether the data
+        # extracted here is sensible. Please do improve this if
+        # needed!
+        $info{ $symbol, "symbol" }   = $found[1];
+        $info{ $symbol, "isin" }     = $found[2];
+        $info{ $symbol, "price" }    = hu_decimal( $found[3] );
+        $info{ $symbol, "open" }     = hu_decimal( $found[8] );
+        $info{ $symbol, "close" }    = hu_decimal( $found[9] );
+        $info{ $symbol, "currency" } = $found[10];
+        $info{ $symbol, "low" }      = hu_decimal( $found[13] );
+        $info{ $symbol, "high" }     = hu_decimal( $found[14] );
+        $info{ $symbol, "p_change" } = hu_decimal( $found[16] );
+        $info{ $symbol, "last" }     = hu_decimal( $found[17] );
+        $quoter->store_date( \%info, $symbol, { isodate => $found[18] } );
 
-	$info{$symbol, "success"} = 1;
+        $info{ $symbol, "success" } = 1;
     }
 
     return wantarray() ? %info : \%info;
 }
 
 sub bamosz {
-    my $quoter = shift;
+    my $quoter  = shift;
     my @symbols = @_;
     my %info;
 
     my $ua = $quoter->user_agent;
 
     for my $symbol (@symbols) {
-	$info{$symbol, "method"} = "bamosz";
-	$info{$symbol, "source"} = $BAMOSZ_MAINURL;
-	$info{$symbol, "success"} = 0;
+        $info{ $symbol, "method" }  = "bamosz";
+        $info{ $symbol, "source" }  = $BAMOSZ_MAINURL;
+        $info{ $symbol, "success" } = 0;
 
-	my $url = $BAMOSZ_URL . $symbol;
-	my $response = $ua->request(GET $url);
-	unless ($response->is_success) {
-	    $info{$symbol, "errormsg"} = "Request error";
-	    next;
-	}
+        my $url      = $BAMOSZ_URL . $symbol;
+        my $response = $ua->request( GET $url);
+        unless ( $response->is_success ) {
+            $info{ $symbol, "errormsg" } = "Request error";
+            next;
+        }
 
-	my $te = HTML::TableExtract->new(attribs => {class => "dataTable"});
-	$te->parse(decode_utf8($response->content));
-	unless ($te->first_table_found) {
-	    $info{$symbol, "errormsg"} = "No dataTable found";
-	    next;
-	}
+        my $te = HTML::TableExtract->new( attribs => { class => "dataTable" } );
+        $te->parse( decode_utf8( $response->content ) );
+        unless ( $te->first_table_found ) {
+            $info{ $symbol, "errormsg" } = "No dataTable found";
+            next;
+        }
 
-	my $ts = $te->table(0, 0);
-	$info{$symbol, "name"} = $ts->cell(0, 1);
-	my $isin = $ts->cell(2, 1);
-	$info{$symbol, "symbol"} = $isin;
-	$info{$symbol, "isin"} = $isin;
-	$info{$symbol, "currency"} = $ts->cell(3, 1);
-	my $price = hu_decimal($ts->cell(5, 1));
-	$info{$symbol, "price"} = $price;
-	$info{$symbol, "last"} = $price;
-	my $date = $ts->cell(6, 1);
-	$quoter->store_date(\%info, $symbol, {isodate => $date});
+        my $ts = $te->table( 0, 0 );
+        $info{ $symbol, "name" } = $ts->cell( 0, 1 );
+        my $isin = $ts->cell( 2, 1 );
+        $info{ $symbol, "symbol" }   = $isin;
+        $info{ $symbol, "isin" }     = $isin;
+        $info{ $symbol, "currency" } = $ts->cell( 3, 1 );
+        my $price = hu_decimal( $ts->cell( 5, 1 ) );
+        $info{ $symbol, "price" } = $price;
+        $info{ $symbol, "last" }  = $price;
+        my $date = $ts->cell( 6, 1 );
+        $quoter->store_date( \%info, $symbol, { isodate => $date } );
 
-	$info{$symbol, "success"} = 1;
+        $info{ $symbol, "success" } = 1;
     }
 
     return wantarray() ? %info : \%info;
@@ -174,22 +191,24 @@ sub bamosz {
 sub trim {
     my $s = shift;
     if ($s) {
-	$s =~ s/^\s+//;
-	$s =~ s/\s+$//;
-	return $s;
-    } else {
-	return '';
+        $s =~ s/^\s+//;
+        $s =~ s/\s+$//;
+        return $s;
+    }
+    else {
+        return '';
     }
 }
 
 sub hu_decimal {
     my $s = shift;
     if ($s) {
-	$s =~ s/[^\d,-]//g;
-	$s =~ s/,/./;
-	return $s;
-    } else {
-	return '';
+        $s =~ s/[^\d,-]//g;
+        $s =~ s/,/./;
+        return $s;
+    }
+    else {
+        return '';
     }
 }
 
--- t/hu.t
+++ t/hu.t
@@ -12,7 +12,7 @@ use strict;
 use Test::More;
 use Finance::Quote;
 
-if (not $ENV{ONLINE_TEST}) {
+if ( not $ENV{ONLINE_TEST} ) {
     plan skip_all => 'Set $ENV{ONLINE_TEST} to run this test';
 }
 
@@ -20,47 +20,48 @@ plan tests => 21;
 
 # Test za functions.
 
-my $q      = Finance::Quote->new("HU");
-my $year   = (localtime())[5] + 1900;
+my $q        = Finance::Quote->new("HU");
+my $year     = ( localtime() )[5] + 1900;
 my $lastyear = $year - 1;
 
-#getting quotes for shares by ticker (OTP,ANY,RABA), 
-# shares by ISIN (ticker: MTELEKOM: HU0000073507, MOL: HU0000068952) 
+#getting quotes for shares by ticker (OTP,ANY,RABA),
+# shares by ISIN (ticker: MTELEKOM: HU0000073507, MOL: HU0000068952)
 # and for funds by ISIN (HU0000702709,HU0000706437)
 # and finally an incorrect ticker/isin is ZOL, must fail.
-my %quotes = $q->hu("OTP","HU0000068952","HU0000702709","ZOL"); #,"ANY","RABA","HU0000073507","HU0000068952","HU0000702709","HU0000706437","ZOL");
+my %quotes = $q->hu( "OTP", "HU0000068952", "HU0000702709", "ZOL" )
+    ; #,"ANY","RABA","HU0000073507","HU0000068952","HU0000702709","HU0000706437","ZOL");
 ok(%quotes);
 
 # Check that the last and date values are defined.
-ok($quotes{"OTP","success"});
-ok($quotes{"OTP","last"} > 0);
-ok(length($quotes{"OTP","date"}) > 0);
-ok(substr($quotes{"OTP","isodate"},0,4) == $year ||
-   substr($quotes{"OTP","isodate"},0,4) == $lastyear);
-ok(substr($quotes{"OTP","date"},6,4) == $year ||
-   substr($quotes{"OTP","date"},6,4) == $lastyear);
-ok($quotes{"OTP","currency"} eq "HUF");
+ok( $quotes{ "OTP", "success" } );
+ok( $quotes{ "OTP", "last" } > 0 );
+ok( length( $quotes{ "OTP", "date" } ) > 0 );
+ok(    substr( $quotes{ "OTP", "isodate" }, 0, 4 ) == $year
+    || substr( $quotes{ "OTP", "isodate" }, 0, 4 ) == $lastyear );
+ok(    substr( $quotes{ "OTP", "date" }, 6, 4 ) == $year
+    || substr( $quotes{ "OTP", "date" }, 6, 4 ) == $lastyear );
+ok( $quotes{ "OTP", "currency" } eq "HUF" );
 
 # MTELEKOM: HU0000073507
-ok($quotes{"HU0000068952","success"});
-ok($quotes{"HU0000068952","last"} > 0);
-ok(length($quotes{"HU0000068952","date"}) > 0);
-ok(substr($quotes{"HU0000068952","isodate"},0,4) == $year ||
-   substr($quotes{"HU0000068952","isodate"},0,4) == $lastyear);
-ok(substr($quotes{"HU0000068952","date"},6,4) == $year ||
-   substr($quotes{"HU0000068952","date"},6,4) == $lastyear);
-ok($quotes{"HU0000068952","currency"} eq "HUF");
+ok( $quotes{ "HU0000068952", "success" } );
+ok( $quotes{ "HU0000068952", "last" } > 0 );
+ok( length( $quotes{ "HU0000068952", "date" } ) > 0 );
+ok(    substr( $quotes{ "HU0000068952", "isodate" }, 0, 4 ) == $year
+    || substr( $quotes{ "HU0000068952", "isodate" }, 0, 4 ) == $lastyear );
+ok(    substr( $quotes{ "HU0000068952", "date" }, 6, 4 ) == $year
+    || substr( $quotes{ "HU0000068952", "date" }, 6, 4 ) == $lastyear );
+ok( $quotes{ "HU0000068952", "currency" } eq "HUF" );
 
 # Fund: Budapest II, isin: HU0000702709
-ok($quotes{"HU0000702709","success"});
-ok($quotes{"HU0000702709","last"} > 0);
-ok(length($quotes{"HU0000702709","date"}) > 0);
-ok(substr($quotes{"HU0000702709","isodate"},0,4) == $year ||
-   substr($quotes{"HU0000702709","isodate"},0,4) == $lastyear);
-ok(substr($quotes{"HU0000702709","date"},6,4) == $year ||
-   substr($quotes{"HU0000702709","date"},6,4) == $lastyear);
-ok($quotes{"HU0000702709","currency"} eq "HUF");
+ok( $quotes{ "HU0000702709", "success" } );
+ok( $quotes{ "HU0000702709", "last" } > 0 );
+ok( length( $quotes{ "HU0000702709", "date" } ) > 0 );
+ok(    substr( $quotes{ "HU0000702709", "isodate" }, 0, 4 ) == $year
+    || substr( $quotes{ "HU0000702709", "isodate" }, 0, 4 ) == $lastyear );
+ok(    substr( $quotes{ "HU0000702709", "date" }, 6, 4 ) == $year
+    || substr( $quotes{ "HU0000702709", "date" }, 6, 4 ) == $lastyear );
+ok( $quotes{ "HU0000702709", "currency" } eq "HUF" );
 
 # Check that a ZOL fund returns no-success.
-ok(!$quotes{"ZOL","success"});
-ok($quotes{"ZOL","errormsg"} eq "Fetch from bse or bamosz failed");
+ok( !$quotes{ "ZOL", "success" } );
+ok( $quotes{ "ZOL",  "errormsg" } eq "Fetch from bse or bamosz failed" );
