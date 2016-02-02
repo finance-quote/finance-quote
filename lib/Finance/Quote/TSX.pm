07566ac6051f73a1ac743516b6c70809ef8e3d37
Bob Swift <bswift@rsds.ca>
2016-02-02 15:50:02 -0700

    Update TSX Module
    Update the tsx.pm module to accommodate the change made to the search
    URL and protocol for the information, and to properly scrape the
    information from the updated format web pages returned.
    
    Changes include:
    
    - Updated the URL used, and symbol request protocol
    - Updated scraper for the search web page returned
    - Added currency, date, time and symbol to labels returned
    - Added check for symbols returned from the search in a different case
    than requested

--- lib/Finance/Quote/TSX.pm
+++ lib/Finance/Quote/TSX.pm
@@ -8,6 +8,7 @@
 #    Copyright (C) 2001, Leigh Wedding <leigh.wedding@telstra.com>
 #    Copyright (C) 2006, Mika Laari <mika.laari@iki.fi>
 #    Copyright (C) 2008, Emmanuel Rodriguez <potyl@cpan.org>
+#    Copyright (C) 2016, Bob Swift <bswift@rsds.ca>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -44,12 +45,20 @@ use URI::QueryParam;
 # VERSION
 
 # This URL is able to accept up to 10 symbols at a time
-my $TSX_URL = URI->new('http://cxa.marketwatch.com/tsx/en/market/getquote.aspx');
+my $TSX_URL = 'http://web.tmxmoney.com/getquote.php';
+my $param = 'symbols[]=';
+
+# Example URL for multiple quotes: 'http://web.tmxmoney.com/getquote.php?symbols[]=stn&symbols[]=ap.un&symbols[]=pca002&symbols[]=tsx&symbols[]=notvalid';
+#                                                                                   ^               ^               ^              ^               ^
+#                                                                                 Stock           Stock            Fund          Index         (invalid)
 
 # The number of symbols that can be fetched at a time per URL
 my $BATCH_SIZE = 10;
 
-my @LABELS = qw(name last net p_change volume exchange);
+my @LABELS = qw(name last date time net p_change volume exchange currency symbol);
+
+# Symbol Map
+my %symbolmap = ();
 
 
 sub methods {
@@ -77,7 +86,14 @@ sub tsx {
 	my $quoter = shift;
 	my @symbols = @_;
 	return unless @symbols;
+    
+    # Save a copy of the symbols list for later checking
+    my @save_symbols = @symbols;
 
+    # Symbols returned on the web site are all upper case regardless of the case searched,
+    # so we create a map of the symbols input to the search to an upper case version of
+    # the symbol.  The original search symbol will be used as the key in the hash returned.
+    %symbolmap = map { uc($_) => $_ } @symbols;
 	my %info = ();
 
 	# Fetch the stocks per batch
@@ -87,8 +103,7 @@ sub tsx {
 		my @batch = splice @symbols, 0, $BATCH_SIZE;
 
 		# Build the URL
-		my $url = $TSX_URL->clone;
-		$url->query_param(symb => join ',', @batch);
+		my $url = $TSX_URL . '?' . $param . join('&' . $param, @batch);
 
 		# Download the stock information
 		my $response = $quoter->user_agent->get($url);
@@ -104,14 +119,18 @@ sub tsx {
 		extract_stock_data(\%info, $response->content);
 	}
 
-
-	# Make sure that all symbols where found
-	foreach my $symbol (@symbols) {
-		next if exists $info{$symbol, 'name'};
+	# Make sure that all symbols were found
+    # and clean up date and time information.
+	foreach my $symbol (@save_symbols) {
+        if ((defined $info{$symbol,"date"}) && ($info{$symbol,"date"} ne "")) {
+            $quoter->store_date(\%info, $symbol, {usdate => $info{$symbol,"date"}}); }
+        if ((defined $info{$symbol,"time"}) && ($info{$symbol,"time"} ne "")) {
+            $info{$symbol, "time"} = $quoter->isoTime($info{$symbol,"time"}); }
+        next if exists $info{$symbol, 'name'};
 		$info{$symbol, 'success'} = 0;
 		$info{$symbol, 'errormsg'} = "Symbol not found";
 	}
-
+    
 	return wantarray() ? %info : \%info;
 }
 
@@ -125,49 +144,102 @@ sub extract_stock_data {
 
 	my ($info, $content) = @_;
 
-	# The stocks are in <table class="data">
+    # Get the dates for Stocks and Funds because they appear in different tables.
+    # Get the dates for Indices if the Stocks table is missing.  Stocks dates and
+    # Indices dates are shown as the same on the web page returned from a search.
+    # Use Stocks dates by default for everything except Funds.
+    my ($s_date, $s_time) = getdate('Stocks', $content);
+    my ($f_date, $f_time) = getdate('Funds', $content);
+    if ($s_date eq "") { ($s_date, $s_time) = getdate('Indices', $content); }
+    
+	# The stocks, funds and indices are in separate tables of <table class="tablemaster">
+    # thus we need to check three counts of the table to catch everything.
 	# NOTE: TSX returns extra symbols who's names are similar to the symbols that
 	#       are going to be fetched. This method will exclude such symbols.
-	my $parser = HTML::TableExtract->new(attribs => { class => 'data' } );
-	$parser->parse($content);
+    my $counter = 0;
+    while ($counter < 3) {
+        my $parser = HTML::TableExtract->new(count => $counter++, attribs => { class => 'tablemaster' } );
+        $parser->parse($content);
+    
+        my ($table) = $parser->tables;
+        return unless defined $table;
+    
+        my $is_header = 1;
+        foreach my $row ($table->rows) {
+    
+            # Skip the header
+            if ($is_header) {
+                $is_header = 0;
+                next;
+            }
+    
+            # Initialize field counter
+            my $i = 0;
+            
+            # Parse the symbol, and compare to symbol map
+            my $symbolread = uc($row->[$i++]);
+            my $symbol = $symbolread;
+            if (exists $symbolmap{$symbol}) { $symbol = $symbolmap{$symbol}; }
+    
+            # Parse the name and last price
+            my $name = $row->[$i++];
+            my $last = $row->[$i++];
+            $last =~ s/^\s*//;
+            if ($last eq "") { $last = 0; }
+            
+            # Skip if not a valid symbol
+            next unless ((defined $name) && ($name ne "") && ($last > 0));
+            
+            # Set currency based on ":US" extension to symbol
+            my $currency = 'CAD';
+            if ($symbol =~ m/:US$/i) { $currency = 'USD'; }
+            $info->{$symbol, 'currency'} = $currency;
+    
+            # Set exchange based on lack of ":US" extension to symbol
+            my $exchange = 'T';
+            if ($symbol =~ m/:US$/i) { $exchange = ''; }
+            $info->{$symbol, 'exchange'} = $exchange;
+    
+            # Save previously parsed field information and parse the other fields
+            $info->{$symbol, 'symbol'} = $symbolread;
+            $info->{$symbol, 'name'} = $name;
+            $info->{$symbol, 'last'} = $last;
+            # The change is in the format '-0.40 (-0.5%)' (positive numbers have no sign)
+            if ($row->[$i++] =~ /(-?\d+\.\d+) \s+ \((-?\d+\.\d+)%\)/x) {
+                $info->{$symbol, 'net'} = $1;
+                $info->{$symbol, 'p_change'} = $2;
+            }
+            my $volume = $row->[$i++];
+            if ((not defined $volume) or ($volume eq "")) {
+                # Use Funds date and time
+                $info->{$symbol, 'date'} = $f_date;
+                $info->{$symbol, 'time'} = $f_time;
+                $volume = 0;
+            } else {
+                # Use Stocks date and time
+                $info->{$symbol, 'date'} = $s_date;
+                $info->{$symbol, 'time'} = $s_time;
+            }
+            $info->{$symbol, 'volume'} = $volume;
+            
+            # Cleanup
+            $info->{$symbol, 'volume'} =~ s/,//g;
+            $info->{$symbol, 'method'} = 'tsx';
+            $info->{$symbol, 'success'} = 1;
+        }
+    }
+}
 
-	my ($table) = $parser->tables;
-	return unless defined $table;
 
-	my $is_header = 1;
-	foreach my $row ($table->rows) {
-
-		# Skip the header
-		if ($is_header) {
-			$is_header = 0;
-			next;
-		}
-
-		my $i = 0;
-		# The symbol is in the format 'BCE-T' (T is the exchange)
-		my $symbol = $row->[$i++];
-		if ($symbol =~ s/-(.)$//) {
-			$info->{$symbol, 'exchange'} = $1;
-		}
-
-
-		# Parse the other fields
-		$info->{$symbol, 'name'} = $row->[$i++];
-		$info->{$symbol, 'last'} = $row->[$i++];
-		# The change is in the format '-0.40 (-0.5%)' (positive numbers have no sign)
-		if ($row->[$i++] =~ /(-?\d+\.\d+) \s+ \((-?\d+\.\d+)%\)/x) {
-			$info->{$symbol, 'net'} = $1;
-			$info->{$symbol, 'p_change'} = $2;
-		}
-		$info->{$symbol, 'volume'} = $row->[$i++];
-
-
-		# Cleanup
-		$info->{$symbol, 'volume'} =~ s/,//g;
-
-		$info->{$symbol, 'method'} = 'tsx';
-		$info->{$symbol, 'success'} = 1;
-	}
+# Parse date and time information and return in standard format for later processing
+sub getdate {
+	my ($dtype, $content) = @_;
+    my ($datetime) = ($content =~ m/>$dtype<[^>]+.[^>]+.([^<]*)/i);
+    unless ((defined $datetime) && ($datetime ne "")) { return ("", ""); }
+    $datetime =~ s/,//g;
+    my ($mo, $da, $yr, $tm) = ($datetime =~ m/^\s*(\S+)\s+(\S+)\s+(\S+)\s+(.*)\s*$/);
+    my $dt = sprintf("%s %u %u", $mo, $da, $yr);
+    return ($dt, $tm);
 }
 
 
@@ -183,19 +255,19 @@ Finance::Quote::TSX	- Obtain quotes from the Toronto Stock Exchange.
 
     $q = Finance::Quote->new;
 
-    %stockinfo = $q->fetch("tsx","NT-T");	  # Only query TSX.
-    %stockinfo = $q->fetch("canada","NT");  # Failover to other sources OK.
+    %stockinfo = $q->fetch("tsx","BCE");    # Only query TSX.
+    %stockinfo = $q->fetch("canada","BCE"); # Failover to other sources OK.
 
 =head1 DESCRIPTION
 
 This module obtains information from the Toronto Stock Exchange
-through the page http://www.TMXmoney.com/.
+through the page http://web.TMXmoney.com/.
 
 This module is not loaded by default on a Finance::Quote object.
 It's possible to load it explicity by placing "TSX" in the argument
 list to Finance::Quote->new().
 
-This module provides both the "tsx" and "toronto" fetch methods.
+This module provides both the "tsx" and "canada" fetch methods.
 Please use the "canada" fetch method if you wish to have failover with other
 sources for Canadian stocks.  Using the "tsx" method will guarantee that your
 information only comes from the Toronto Stock Exchange.
@@ -203,7 +275,8 @@ information only comes from the Toronto Stock Exchange.
 =head1 LABELS RETURNED
 
 The following labels are returned by Finance::Quote::TSX:
-name, last, net, p_change, volume and exchange.
+name, last, date, time, net, p_change, volume, and exchange,
+plus two additional custom labels currency and symbol.
 
 This module returns less information (labels) than other sources but it's able
 to retrieve them faster because each HTTP request performed can return up to 10
