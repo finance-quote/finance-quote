7b9d2e7474fa121c194e33317240cb4e03e77f44
Matthew Patterson <matthew.s.patterson@gmail.com>
2017-11-03 22:32:18 -0500

    Initial pass at adding AlphaVantage module to Finance::Quote
    All tests pass, and testing with the stockdump.pl script has proven
    successful, but I have not tried to incorporate this into GnuCash yet.

---
+++ lib/Finance/Quote/AlphaVantage.pm
@@ -0,0 +1,65 @@
+#!/usr/bin/perl -w
+
+package Finance::Quote::AlphaVantage;
+
+require 5.005;
+
+use strict;
+use JSON qw( decode_json );
+use HTTP::Request::Common;
+
+my $ALPHAVANTAGE_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=1min';
+my $ALPHAVANTAGE_API_KEY = $ENV{"ALPHAVANTAGE_API_KEY"};
+
+die "Expected ALPHAVANTAGE_API_KEY to be set; get an API key at https://www.alphavantage.co" unless (defined $ALPHAVANTAGE_API_KEY);
+
+sub methods {
+    return ( alphavantage => \&alphavantage,
+    );
+
+    my @labels = qw/date isodate open high low close volume/;
+
+    sub labels {
+        return ( alphavantage => \@labels,
+        );
+    }
+}
+
+sub alphavantage {
+    my $quoter = shift;
+    my @stocks = @_;
+    my ( %info, $reply, $url );
+    my $ua = $quoter->user_agent();
+
+    foreach my $stock (@stocks) {
+        $url = $ALPHAVANTAGE_URL . '&apikey=' . $ALPHAVANTAGE_API_KEY . '&symbol=' . $stock;
+        $reply = $ua->request(GET $url);
+
+        my $code = $reply->code;
+        my $desc = HTTP::Status::status_message($code);
+        my $body = $reply->content;
+
+        my $json_data = JSON::decode_json $body;
+        my ( $latest );
+
+        if ($json_data->{"Time Series (1min)"}) {
+            $info{ $stock, "success" } = 1;
+
+            $latest = (keys($json_data->{"Time Series (1min)"}))[0];
+
+            $info{ $stock, "open" } = $json_data->{"Time Series (1min)"}->{$latest}->{"1. open"};
+            $info{ $stock, "close" } = $json_data->{"Time Series (1min)"}->{$latest}->{"4. close"};
+            $info{ $stock, "high" } = $json_data->{"Time Series (1min)"}->{$latest}->{"2. high"};
+            $info{ $stock, "low" } = $json_data->{"Time Series (1min)"}->{$latest}->{"3. low"};
+            $info{ $stock, "volume" } = $json_data->{"Time Series (1min)"}->{$latest}->{"5. volume"};
+            $info{ $stock, "isodate" } = substr($latest, 0, 10);
+
+            $quoter->store_date( \%info, $stock, { isodate => $latest });
+        } else {
+            $info{ $stock, "success" } = 0;
+            $info{ $stock, "errormsg" } = $json_data->{"Error Message"} || $json_data->{"Information"};
+        }
+    }
+
+    return wantarray() ? %info : \%info;
+}
---
+++ t/alphavantage.t
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+use strict;
+use Test::More;
+use Finance::Quote;
+
+if (not $ENV{"ONLINE_TEST"}) {
+    plan skip_all => 'Set $ENV{ONLINE_TEST} to run this test';
+}
+
+if (not $ENV{"ALPHAVANTAGE_API_KEY"}) {
+    plan skip_all => 'Set $ENV{ALPHAVANTAGE_API_KEY} to run this test; get one at https://www.alphavantage.co';
+}
+
+plan tests => 14;
+
+my $q = Finance::Quote->new();
+my $year = (localtime())[5] + 1900;
+my $lastyear = $year - 1;
+
+my %quotes = $q->alphavantage("IBM","CSCO","BOGUS");
+ok(%quotes);
+
+ok($quotes{"IBM","success"});
+ok($quotes{"IBM","open"} > 0);
+ok($quotes{"IBM","close"} > 0);
+ok($quotes{"IBM","high"} > 0);
+ok($quotes{"IBM","low"} > 0);
+ok($quotes{"IBM","volume"} > 0);
+ok(substr($quotes{"IBM","isodate"},0,4) == $year ||
+    substr($quotes{"IBM","isodate"},0,4) == $lastyear);
+ok(substr($quotes{"IBM","date"},6,4) == $year ||
+    substr($quotes{"IBM","date"},6,4) == $lastyear);
+
+ok($quotes{"CSCO","success"});
+ok($quotes{"CSCO","open"} > 0);
+ok($quotes{"CSCO","close"} > 0);
+ok($quotes{"CSCO","volume"} > 0);
+
+ok(! $quotes{"BOGUS","success"});
