64a0d5a1f75c2fcd52aa4337120578474e1bfbf2
e-dorigatti <emilio.dorigatti@gmail.com>
2020-09-30 20:06:27 +0200

    optionally specifying exchange

--- lib/Finance/Quote/XETRA.pm
+++ lib/Finance/Quote/XETRA.pm
@@ -26,10 +26,8 @@ use HTTP::Request::Common;
 use HTML::TableExtract;
 use Time::Piece;
 
-# VERSION
 
-
-my $PRICE_URL = "https://api.boerse-frankfurt.de/data/price_information?mic=XETR&isin=";
+my $PRICE_URL = "https://api.boerse-frankfurt.de/data/price_information";
 
 sub methods {
     return ( xetra => \&xetra,
@@ -55,7 +53,23 @@ sub xetra {
     $ua->max_size(10);
 
     foreach my $stocks (@stocks) {
-        my $url = $PRICE_URL . $stocks;
+        my @parts = split /\./, $stocks;
+        my $isin = $parts[0];
+
+        my $mic;
+        if ( scalar @parts == 1 ) {
+            $mic = "XETR";
+        }
+        elsif ( scalar @parts == 2 ) {
+            $mic = $parts[1];
+        }
+        else {
+            $info{ $stocks, "success" } = 0;
+            $info{ $stocks, "errormsg" } = "Invalid format: use only one '.' to separate ISIN from MIC";
+            continue;
+        }
+
+        my $url = $PRICE_URL . "?mic=" . $mic . "&isin=" . $isin;
         my $resp = $ua->request(GET $url);
         my $code = $resp->code;
         my $desc = HTTP::Status::status_message($code);
@@ -69,7 +83,7 @@ sub xetra {
 
             $info{$stocks, 'success'} = 1;
             $info{$stocks, 'method'} = 'xetra';
-            $info{$stocks, 'exchange'} = 'XETR';
+            $info{$stocks, 'exchange'} = $mic;
             $info{$stocks, 'symbol'} = $data->{'isin'};
             $info{$stocks, 'last'} = $data->{'lastPrice'};
             $info{$stocks, 'high'} = $data->{'dayHigh'};
@@ -98,7 +112,7 @@ sub xetra {
 
 =head1 NAME
 
-Finance::Quote::XETRA - Obtain quotes from XETRA.
+Finance::Quote::XETRA - Obtain quotes from XETRA/XFRA.
 
 =head1 SYNOPSIS
 
@@ -106,15 +120,21 @@ Finance::Quote::XETRA - Obtain quotes from XETRA.
 
     $q = Finance::Quote->new;
 
-    %info = Finance::Quote->fetch("xetra","EU0009658145");
+    %info = Finance::Quote->fetch("xetra", "EU0009658145.XETR");
 
 =head1 DESCRIPTION
 
-This module fetches information from the XETRA exchange, also including ETFs.
+This module fetches information from the XETRA and XFRA exchange, also including ETFs.
 
-This module is loaded by default on a Finance::Quote object. It's
-also possible to load it explicitly by placing "XETRA" in the argument
-list to Finance::Quote->new().
+The desired exchange can be specified by appending 'XETR' or 'XFRA' after the ISIN,
+separating the two with a dot '.' (see synopsys).
+
+The full list of available symbols can be found at
+https://www.xetra.com/xetra-de/instrumente/alle-handelbaren-instrumente
+
+This module is loaded by default on a Finance::Quote object. It's also possible
+to load it explicitly by placing "XETRA" in the argument list to
+Finance::Quote->new().
 
 This module provides the "xetra" fetch method.
 
