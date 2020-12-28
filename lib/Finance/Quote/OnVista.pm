37c9e51223aebdf7371c4f0f5df64412bcd50e47
Vincent Lucarelli <vincent.lucarelli@gmail.com>
2020-12-28 18:27:20 -0500

    Fix critic warning

--- lib/Finance/Quote/OnVista.pm
+++ lib/Finance/Quote/OnVista.pm
@@ -34,13 +34,13 @@
 package Finance::Quote::OnVista;
 require 5.005;
 
-# VERSION
-
 use strict;
 use LWP::UserAgent;
 use HTTP::Request::Common;
 use HTML::TreeBuilder;
 use Encode;
+
+# VERSION
 
 use vars qw($OnVista_URL);
 
