23e59666ecd5416e110bcf7f9eddaa3ee6bb74b4
CleanShed <72849657+CleanShed@users.noreply.github.com>
2021-01-14 21:20:12 +1100

    ASX website revamp, use JSON sources instead of HTML scraping.
    October 2020 the ASX revamped their website with dynamic content for quotes
    which prevented the previous HTML screen scraping from working, but exposed
    a number of JSON data sources, two of which are used here.
    The primary source returns data elements for almost all securities, but
    does not return prices for certain security types (some bonds and exchange
    traded products, options, and warrants), and returns an error for indices.
    The alternate source returns less data elements, but provides usable quote
    data for all the known exceptions, including indices.
    This version will always call the primary source, and call the alternate if
    a price is not returned by the primary.
    
    Smart::Comments implemented to conform with the Hackers Guide:
    https://github.com/finance-quote/finance-quote/blob/master/Documentation/Hackers-Guide

--- lib/Finance/Quote/ASX.pm
+++ lib/Finance/Quote/ASX.pm
@@ -1,27 +1,27 @@
 #!/usr/bin/perl -w
 #
-#    Copyright (C) 1998, Dj Padzensky <djpadz@padz.net>
-#    Copyright (C) 1998, 1999 Linas Vepstas <linas@linas.org>
-#    Copyright (C) 2000, Yannick LE NY <y-le-ny@ifrance.com>
-#    Copyright (C) 2000, Brent Neal <brentn@users.sourceforge.net>
-#    Copyright (C) 2001, Leigh Wedding <leigh.wedding@telstra.com>
-#    Copyright (C) 2000-2004, Paul Fenwick <pjf@cpan.org>
-#    Copyright (C) 2014, Chris Good <chris.good@@ozemail.com.au>
+#	Copyright (C) 1998, Dj Padzensky <djpadz@padz.net>
+#	Copyright (C) 1998, 1999 Linas Vepstas <linas@linas.org>
+#	Copyright (C) 2000, Yannick LE NY <y-le-ny@ifrance.com>
+#	Copyright (C) 2000, Brent Neal <brentn@users.sourceforge.net>
+#	Copyright (C) 2001, Leigh Wedding <leigh.wedding@telstra.com>
+#	Copyright (C) 2000-2004, Paul Fenwick <pjf@cpan.org>
+#	Copyright (C) 2014, Chris Good <chris.good@@ozemail.com.au>
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+#	This program is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; either version 2 of the License, or
+#	(at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-#    02110-1301, USA
+#	You should have received a copy of the GNU General Public License
+#	along with this program; if not, write to the Free Software
+#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#	02110-1301, USA
 #
 #
 # This code derived from Padzensky's work on package Finance::YahooQuote,
@@ -39,186 +39,350 @@ package Finance::Quote::ASX;
 use LWP::UserAgent;
 use JSON qw/decode_json/;
 
-use vars qw/$ASX_URL $ASX_URL_FALLBACK/;
+use constant DEBUG => $ENV{DEBUG};
+use if DEBUG, 'Smart::Comments';
+### ASX.pm  Debugging is switched on for <file>[<line>]
 
-# VERSION
+use vars qw/$ASX_URL_PRIMARY $ASX_URL_ALTERNATE/;
 
-$ASX_URL = 'https://www.asx.com.au/asx/1/share';
-$ASX_URL_FALLBACK =
-    'https://asx.api.markitdigital.com/asx-research/1.0/companies';
+##our $VERSION = '1.50'; # VERSION
 
+$ASX_URL_PRIMARY = 'https://www.asx.com.au/asx/1/share/';
+$ASX_URL_ALTERNATE = 'https://asx.api.markitdigital.com/asx-research/1.0/companies/';
+
+=item methods
+
+# Function that lists the methods available for the Australian Securities Exchange (ASX)
+
+=cut
 
 sub methods {return (australia => \&asx,asx => \&asx)}
 
 {
-    my @labels = qw/
-        last
-        net
-        p_change
-        bid
-        offer
-        open
-        close
-        high
-        low
-        volume
-        price
-        method
-        exchange/;
+	my @labels = qw/
+			ask
+			bid
+			cap
+			close
+			date
+			eps
+			high
+			last
+			low
+			name
+			net
+			open
+			p_change
+			pe
+			type
+			volume/;
 
-    sub labels { return (australia => \@labels,
-                         asx       => \@labels); }
+=item labels
+
+# Function that lists the data items available from the Australian Securities Exchange (ASX)
+
+=cut
+
+	sub labels { return (australia => \@labels,
+						 asx	   => \@labels); }
 }
 
 # Australian Stock Exchange (ASX)
-# The ASX provides free delayed quotes through their webpage.
+# The ASX provides free delayed quotes through their webpage:
+#	https://www2.asx.com.au/markets/company/NAB
 #
 # Maintainer of this section is Paul Fenwick <pjf@cpan.org>
 # 5-May-2001 Updated by Leigh Wedding <leigh.wedding@telstra.com>
 # 24-Feb-2014 Updated by Chris Good <chris.good@@ozemail.com.au>
 # 12-Oct-2020 Updated by Jeremy Volkening
+# Jan-2021 Updated by Geoff <cleanoutmyshed@gmail.com>
+#	October 2020 the ASX revamped their website with dynamic content for quotes
+#	which prevented the previous HTML screen scraping from working, but exposed
+#	a number of JSON data sources, two of which are used here.
+#	The primary source returns data elements for almost all securities, but
+#	does not return prices for certain security types (some bonds and exchange
+#	traded products, options, and warrants), and returns an error for indices.
+#	The alternate source returns less data elements, but provides usable quote
+#	data for all the known exceptions, including indices.
+#	This version will always call the primary source, and call the alternate if
+#	a price is not returned by the primary.
+#
+#	Smart::Comments implemented to conform with the Hackers Guide:
+#		https://github.com/finance-quote/finance-quote/blob/master/Documentation/Hackers-Guide
+#
+
+=item asx
+
+# Main function to fetch quotes from the Australian Securities Exchange (ASX)
+
+=cut
 
 sub asx {
 
-    my $quoter = shift;
-    my @symbols = @_
-        or return;
+	my $quoter = shift;
+	my @symbols = @_
+		or return;
 
-    my %info;
+	my($error, %info, $status, $ua);
 
-    my $ua = $quoter->user_agent;
+	$ua = $quoter->user_agent;
 
-    SYMBOL:
-    for my $symbol (@symbols) {
+	SYMBOL:
+	for my $symbol (@symbols) {
+### ASX.pm  Processing symbol: $symbol
+		$info{ $symbol, 'symbol'   } = $symbol;
+		$info{ $symbol, 'method'   } = 'asx';
+		$info{ $symbol, 'exchange' } = 'Australian Securities Exchange';
 
-        # there are multiple endpoints returning JSON data on a security. The
-        # primary endpoint returns the most readily-consumable data and works
-        # for most securities, so try that first
+		$symbol =~ s/\s+$//;
+		if	($symbol !~ m/^[A-Za-z0-9]{1,6}$/) {
+			$info{ $symbol, 'success'  } = 0;
+			$info{ $symbol, 'errormsg' } = 'Invalid symbol.  ASX symbols must be alpha numeric maximum length 6 characters.';
+### ASX.pm:  $info{ $symbol, 'errormsg' }
+			next SYMBOL;
+		}
 
-        my $res = $ua->get(
-            join( '/', $ASX_URL, $symbol),
-            'Accept' => 'application/json'
-        );
-        if ($res->header('content-type') =~ /application\/json/) {
-            my $data = decode_json( $res->content );
-            if ($res->is_success && defined $data->{last_price}) {
-                $info{ $symbol, 'success'  } = 1;
-                $info{ $symbol, 'symbol'   } = $symbol;
-                $info{ $symbol, 'last'     } = $data->{last_price};
-                $info{ $symbol, 'net'      } = $data->{change_price};
-                $info{ $symbol, 'p_change' } = $data->{change_in_percent};
-                $info{ $symbol, 'p_change' } =~ s/\%$//; # strip suffix
-                $info{ $symbol, 'bid'      } = $data->{bid_price};
-                $info{ $symbol, 'offer'    } = $data->{offer_price};
-                $info{ $symbol, 'open'     } = $data->{open_price};
-                $info{ $symbol, 'close'    } = $data->{previous_close_price};
-                $info{ $symbol, 'high'     } = $data->{day_high_price};
-                $info{ $symbol, 'low'      } = $data->{day_low_price};
-                $info{ $symbol, 'volume'   } = $data->{volume};
-                $info{ $symbol, 'price'    } = $data->{last_price};
-                $info{ $symbol, 'method'   } = 'asx',
-                $info{ $symbol, 'exchange' } = 'Australian Securities Exchange',
-                $info{ $symbol, 'currency' } = 'AUD',
-                my $date = $data->{last_trade_date};
-                my $t = Time::Piece->strptime($date, '%Y-%m-%dT%H:%M:%S%z');
-                $quoter->store_date(
-                    \%info,
-                    $symbol,
-                    { isodate => $t->ymd }
-                );
-            }
-            else {
-                $info{ $symbol, 'success'  } = 0;
-                $info{ $symbol, 'errormsg' } = "The security $symbol is not"
-                    . " available from this endpoint";
-            }
-        }
-        else {
-            $info{ $symbol, 'success'  } = 0;
-            $info{ $symbol, 'errormsg' } = "Unable to fetch data from the
-            server. HTTP status: " . $res->status_line;
-        }
+		($status, $error) = asx_primary($symbol, $ua, \%info);
 
-        # this secondary endpoint contains the security name, and for a few
-        # securities that fail above may contain limited price data. For
-        # instance, the indexes seem to not be available above but will return
-        # basic data here
+		if	(($status != 1) ||
+			 ($info{$symbol, 'last'} eq '')) {
+### ASX.pm  Calling Alternate URL as Primary URL failed or doesn't have a Last Price
+			($status, $error) = asx_alternate($symbol, $ua, \%info);
+		}
 
-        $res = $ua->get(
-            join( '/', $ASX_URL_FALLBACK, $symbol, 'header'),
-            'Accept' => 'application/json'
-        );
-        if ($res->is_success && $res->header('content-type') =~ /application\/json/) {
-            my $data = decode_json( $res->content )->{data};
-            $info{ $symbol, 'name'     } = $data->{displayName};
-            if (! $info{ $symbol, 'success'  }) {
-                delete $info{ $symbol, 'errormsg'  }; # set previously
-                $info{ $symbol, 'success'  } = 1;
-                $info{ $symbol, 'symbol'   } = $symbol;
-                $info{ $symbol, 'last'     } = $data->{priceLast};
-                $info{ $symbol, 'net'      } = $data->{priceChange};
-                $info{ $symbol, 'p_change' } = $data->{priceChangePercent};
-                $info{ $symbol, 'volume'   } = $data->{volume};
-                $info{ $symbol, 'price'    } = $data->{priceLast};
-                $info{ $symbol, 'method'   } = 'asx';
-                $info{ $symbol, 'exchange' } = 'Australian Securities Exchange';
-                # $info{ $symbol, 'currency' } = 'AUD', # apparently shouldn't be set?
-            }
+		if	($status != 1) {
+			$info{ $symbol, 'success'  } = 0;
+			$info{ $symbol, 'errormsg' } = "$error";
+### ASX.pm  Unsuccessful calls to ASX URLs - symbol cannot be processed: $symbol
+			next SYMBOL;
+		}
 
-        }
-            
-    }
+### ASX.pm  We have valid data, apply various clean ups and add remaining data for symbol: $symbol
 
-    return %info if wantarray;
-    return \%info;
+		# Remove trailing percentage sign from p_change
+		$info{ $symbol, 'p_change' } =~ s/\%$//;
+
+		$info{ $symbol, 'price' } = $info{ $symbol, 'last' };
+
+		if	((exists $info{ $symbol, 'date' }) &&
+			 ($info{ $symbol, 'date' } =~ m/([0-9]{4}-[0-9]{2}-[0-9]{2})T/)) {
+			$quoter->store_date(\%info, $symbol, {isodate => $1});
+### ASX.pm    Converted Last Trade Date to ISO format: "$info{ $symbol, 'date' } --> $1"
+		}
+
+# Technically indices don't have a currency, but it is not possible to distinguish them
+		$info{ $symbol, 'currency' } = 'AUD';
+
+		$info{ $symbol, 'success'  } = 1;
+		$info{ $symbol, 'errormsg' } = '';
+
+	}
+
+### ASX.pm  Returning data for all symbols to Finance-Quote and exiting <file>[<line>]
+	return %info if wantarray;
+	return \%info;
+
+}
+
+=item asx_primary
+
+# Internal function to handle ASX Alternate data source
+
+=cut
+
+sub asx_primary {
+
+	my ($symbol, $ua, $info) = @_;
+
+	my($data, $error, %label_map, $status, $url);
+
+	$url = $ASX_URL_PRIMARY . $symbol;
+	($status, $error, $data) = get_asx_data($url, $ua);
+	return $status, $error unless $status == 1;
+
+# Map the Finance::Quote labels (left) to the corresponding ASX labels (right)
+	%label_map = (
+		'bid'		=> 'bid_price',
+		'p_change'	=> 'change_in_percent',
+		'net'		=> 'change_price',
+		'name'		=> 'desc_full',
+		'high'		=> 'day_high_price',
+		'low'		=> 'day_low_price',
+		'eps'		=> 'eps',
+		'last'		=> 'last_price',
+		'date'		=> 'last_trade_date',
+		'cap'		=> 'market_cap',
+		'ask'		=> 'offer_price',
+		'open'		=> 'open_price',
+		'pe'		=> 'pe',
+		'close'		=> 'previous_close_price',
+		'volume'	=> 'volume',
+	);
+
+	process_asx_data($symbol, $data, \%label_map, $info);
+
+	return 1, '';
+
+}
+
+=item asx_alternate
+
+# Internal function to handle ASX Alternate data source
+
+=cut
+
+sub asx_alternate {
+
+	my ($symbol, $ua, $info) = @_;
+
+	my($data, $error, %label_map, $status, $url);
+
+	$url = $ASX_URL_ALTERNATE . $symbol . '/header';
+	($status, $error, $data) = get_asx_data($url, $ua);
+	return $status, $error unless $status == 1;
+
+	if	(exists $data->{error}) {
+		$status = 0;
+		$error = "Error returned by ASX server '$url'.  Code: " . $data->{error}{code} . '  Message: ' . $data->{error}{message};
+### ASX.pm  Error: $error
+		return $status, $error;
+	}
+
+	if	(! exists $data->{data}) {
+		$status = 0;
+		$error = "Cannot parse content from ASX server '$url'.  Expected a top level JSON element named data.";
+### ASX.pm  Error: $error
+		return $status, $error;
+	}
+
+# Map the Finance::Quote labels (left) to the corresponding ASX labels (right)
+	%label_map = (
+		'name'		=> 'displayName',
+		'ask'		=> 'priceAsk',
+		'bid'		=> 'priceBid',
+		'net'		=> 'priceChange',
+		'p_change'	=> 'priceChangePercent',
+		'last'		=> 'priceLast',
+		'type'		=> 'securityType',
+		'volume'	=> 'volume',
+	);
+
+	process_asx_data($symbol, $data->{data}, \%label_map, $info);
+
+	return 1, '';
+
+}
+
+=item get_asx_data
+
+# Internal function to fetch, validate, and decode data from an ASX URL using LWP User Agent
+# Handle any errors
+
+=cut
+
+sub get_asx_data {
+
+	my ($url, $ua) = @_;
+
+	my($data, $error, $json, $response, $status);
+
+### ASX.pm  Retrieving data from ASX URL: $url
+	$response = $ua->get($url);
+	if	(! $response->is_success) {
+		$status = 0;
+		$error = "Unable to fetch data from the ASX server '$url'.  Status: " . $response->status_line;
+### ASX.pm  Error: $error
+		return $status, $error, undef;
+	}
+
+	if	($response->header('content-type') !~ m|application/json|i) {
+		$status = 0;
+		$error = "Invalid content-type from ASX server '$url'.  Expected: application/json, received: " . $response->header('content-type');
+### ASX.pm  Error: $error
+		return $status, $error, undef;
+	}
+
+	$json = $response->content;
+
+# The JSON module will croak on errors, so use eval to trap this.
+	$data = eval{ decode_json($json) };
+	if	($@) {
+		$status = 0;
+		$error = "Failed to parse JSON data from ASX server '$url'.  Error: '$@'.";
+### ASX.pm  Error: $error
+		return $status, $error, undef;
+	}
+
+# Return valid, decoded data
+	$status = 1;
+	return $status, $error, $data;
+
+}
+
+=item process_asx_data
+
+# Internal function to push the ASX data elements into the Finance::Quote structure (%info)
+
+=cut
+
+sub process_asx_data {
+
+	my ($symbol, $data, $label_map, $info) = @_;
+
+	foreach my $label (sort(keys %{$label_map})) {
+		if	((exists $data->{$label_map->{$label}}) &&
+			 (defined $data->{$label_map->{$label}})) {
+# Concatenate Primary and Alternate Names
+			if	(($label eq 'name') &&
+				 (exists $info->{$symbol, $label}) &&
+				 (uc($info->{$symbol, $label}) ne uc($data->{$label_map->{$label}}))) {
+					$info->{$symbol, $label} = $data->{$label_map->{$label}} . ' ' . $info->{$symbol, $label};
+			}
+# Overwrite all other labels
+			else {
+				$info->{$symbol, $label} = $data->{$label_map->{$label}};
+			}
+### ASX.pm    Mapped ASX data element to Finance-Quote: sprintf("%-22s%-15s%-s", $label_map->{$label}, $label, $data->{$label_map->{$label}})
+		}
+		else {
+			$info->{$symbol,$label} = '';
+		}
+	}
+
+	return;
 
 }
 
 1;
+__END__
 
 =head1 NAME
-
 Finance::Quote::ASX - Obtain quotes from the Australian Stock Exchange.
-
 =head1 SYNOPSIS
-
-    use Finance::Quote;
-
-    $q = Finance::Quote->new;
-
-    %stockinfo = $q->fetch("asx","BHP");       # Only query ASX.
-    %stockinfo = $q->fetch("australia","BHP"); # Failover to other sources OK.
-
+	use Finance::Quote;
+	$q = Finance::Quote->new;
+	%stockinfo = $q->fetch("asx","BHP");	   # Only query ASX.
+	%stockinfo = $q->fetch("australia","BHP"); # Failover to other sources OK.
 =head1 DESCRIPTION
-
 This module obtains information from the Australian Stock Exchange
-http://www.asx.com.au/.  All Australian stocks and indicies are
-available.  Indexes start with the letter 'X'.  For example, the
-All Ordinaries is "XAO".
-
+http://www.asx.com.au/.  Data for all Australian listed securities and indices
+is available.  Indexes start with the letter 'X'.  For example, the
+All Ordinaries is "XAO".  But some securities also start with the letter 'X'.
 This module is loaded by default on a Finance::Quote object.  It's
 also possible to load it explicitly by placing "ASX" in the argument
 list to Finance::Quote->new().
-
 This module provides both the "asx" and "australia" fetch methods.
 Please use the "australia" fetch method if you wish to have failover
 with other sources for Australian stocks (such as Yahoo).  Using
 the "asx" method will guarantee that your information only comes
 from the Australian Stock Exchange.
-
 Information returned by this module is governed by the Australian
 Stock Exchange's terms and conditions.
-
 =head1 LABELS RETURNED
-
 The following labels may be returned by Finance::Quote::ASX:
 bid, offer, open, high, low, last, net, p_change, volume,
 and price.
-
 =head1 SEE ALSO
-
 Australian Stock Exchange, http://www.asx.com.au/
-
 Finance::Quote::Yahoo::Australia.
-
 =cut
