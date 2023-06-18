e064f17fd25fb317986b02af088f1b82361e55de
Achim Winkler <mumpitzstuff@gmail.com>
2023-06-18 11:14:45 +0200

    - close, change and p_change added - bug fixed for values above 999.9 (1.000,0 converted to 1000.0)

--- lib/Finance/Quote/Tradegate.pm
+++ lib/Finance/Quote/Tradegate.pm
@@ -30,16 +30,16 @@ use Web::Scraper;
 
 my $Tradegate_URL = 'https://web.s-investor.de/app/detail.htm?INST_ID=0000057&boerse=TDG&isin=';
 
-sub methods { 
+sub methods {
   return (tradegate => \&tradegate,
-          europe     => \&tradegate); 
+          europe    => \&tradegate);
 }
 
-our @labels = qw/symbol last close exchange volume open price/;
+our @labels = qw/symbol last close exchange volume open price change p_change/;
 
-sub labels { 
+sub labels {
   return (tradegate => \@labels,
-          europe     => \@labels); 
+          europe    => \@labels);
 }
 
 sub tradegate {
@@ -47,66 +47,99 @@ sub tradegate {
   my $ua     = $quoter->user_agent();
   my $agent  = $ua->agent;
   $ua->agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
-  
+
   my %info;
   my $url;
   my $reply;
 
   foreach my $symbol (@_) {
     eval {
-		
-	
-	  my $url = $Tradegate_URL.join('', $symbol);
-	  my $symlen = length($symbol);
-	  
-      my $tree = HTML::TreeBuilder->new_from_url($url);
-	  	  
-	  my $lastvalue = $tree->look_down('class'=>'si_inner_content_box');
 
-	  my $td1 = ($lastvalue->look_down('_tag'=>'td'))[1];
+
+      my $url = $Tradegate_URL.join('', $symbol);
+      my $symlen = length($symbol);
+
+      my $tree = HTML::TreeBuilder->new_from_url($url);
+
+      my $lastvalue = $tree->look_down('class'=>'si_inner_content_box');
+
+      my $td1 = ($lastvalue->look_down('_tag'=>'td'))[1];
       my @child = $td1->content_list;
-	  my $isin =$child[0];
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[3];	
-      @child = $td1->content_list;	
-      my $sharename = $child[0];
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[5];	
+      my $isin =$child[0];
+
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[3];
       @child = $td1->content_list;
-      my $exchange = $child[0];	  
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[7];	
+      my $sharename = $child[0];
+
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[5];
+      @child = $td1->content_list;
+      my $exchange = $child[0];
+
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[7];
       @child = $td1->content_list;
       my $date = substr($child[0], 0, 8);
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[9];	
-	  @child = $td1->content_list;
-      my $price = $child[0];
-      $price =~ s/,/./;
-	  my $encprice = encode_entities($price);	  
-	  my @splitprice= split ('&',$encprice);	  
-      $price = $splitprice[0];
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[11];	
-	  @child = $td1->content_list;
-      my $currency = $child[0];
-      $currency =~ s/Euro/EUR/;	  
-	  
-	  $td1 = ($lastvalue->look_down('_tag'=>'td'))[13];	
-      @child = $td1->content_list;
-      my $volume = $child[0];	  	  
 
-	        
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[9];
+      @child = $td1->content_list;
+      my $price = $child[0];
+      $price =~ s/\.//;
+      $price =~ s/,/\./;
+      my $encprice = encode_entities($price);
+      my @splitprice= split ('&',$encprice);
+      $price = $splitprice[0];
+
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[11];
+      @child = $td1->content_list;
+      my $currency = $child[0];
+      $currency =~ s/Euro/EUR/;
+
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[13];
+      @child = $td1->content_list;
+      my $volume = $child[0];
+
+      $lastvalue = $tree->look_down('id'=>'detailVergleichszahlen');
+
+      #-- change (absolute change)
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[16];
+      @child = $td1->content_list;
+      my $change =$child[0];
+      $change =~ s/\.//;
+      $change =~ s/,/\./;
+      my $encchange = encode_entities($change);
+      my @splitcchange= split ('&',$encchange);
+      $change = $splitcchange[0];
+
+      #-- p_change (relative change)
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[19];
+      @child = $td1->content_list;
+      my $p_change =$child[0];
+      $p_change =~ s/\.//;
+      $p_change =~ s/,/\./;
+
+      #-- close
+      $td1 = ($lastvalue->look_down('_tag'=>'td'))[37];
+      @child = $td1->content_list;
+      my $close =$child[0];
+      $close =~ s/\.//;
+      $close =~ s/,/\./;
+      my $encclose = encode_entities($close);
+      my @splitclose= split ('&',$encclose);
+      $close = $splitclose[0];
+
+
       $info{$symbol, 'success'}   = 1;
       $info{$symbol, 'method'}    = 'Tradegate';
       $info{$symbol, 'symbol'}    = $isin;
-	  $info{$symbol, 'name'}      = $sharename;
-	  $info{$symbol, 'exchange'}  = $exchange;	  
+      $info{$symbol, 'name'}      = $sharename;
+      $info{$symbol, 'exchange'}  = $exchange;
       $info{$symbol, 'last'}      = $price;
       $info{$symbol, 'price'}     = $price;
-      $info{$symbol, 'volume'}     = $volume;	  
+      $info{$symbol, 'close'}     = $close;
+      $info{$symbol, 'change'}    = $change;
+      $info{$symbol, 'p_change'}  = $p_change;
+      $info{$symbol, 'volume'}    = $volume;
       $info{$symbol, 'currency'}  = $currency;
-#      $info{$symbol, 'date'}      = $date;
+#      $info{$symbol, 'date'}     = $date;
       $quoter->store_date(\%info, $symbol, {eurodate => $date});
     };
     if ($@) {
@@ -155,7 +188,7 @@ conditions.
 
 =head1 LABELS RETURNED
 
-The following labels are returned: 
+The following labels are returned:
 currency
 exchange
 last
