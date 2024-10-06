e9c32b0e2c1ccf6977eda338a501dc251bcbfe33
Bruce Schuck <bschuck@asgard-systems.com>
2024-10-06 16:48:55 -0700

    Started rewriting OnVista.pm using template.pm

--- lib/Finance/Quote/OnVista.pm
+++ lib/Finance/Quote/OnVista.pm
@@ -1,15 +1,7 @@
 #!/usr/bin/perl -w
+# vi: set ts=2 sw=2 noai ic showmode showmatch:  
 #
-#    Copyright (C) 1998, Dj Padzensky <djpadz@padz.net>
-#    Copyright (C) 1998, 1999 Linas Vepstas <linas@linas.org>
-#    Copyright (C) 2000, Yannick LE NY <y-le-ny@ifrance.com>
-#    Copyright (C) 2000, Paul Fenwick <pjf@cpan.org>
-#    Copyright (C) 2000, Brent Neal <brentn@users.sourceforge.net>
-#    Copyright (C) 2001, Rob Sessink <rob_ses@users.sourceforge.net>
-#    Copyright (C) 2005, Morten Cools <morten@cools.no>
-#    Copyright (C) 2006, Dominique Corbex <domcox@sourceforge.net>
-#    Copyright (C) 2008, Bernard Fuentes <bernard.fuentes@gmail.com>
-#    Copyright (C) 2009, Erik Colson <eco@ecocode.net>
+#    Copyright (C) 2024, Bruce Schuck <bschuck@asgard-systems.com>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,165 +15,148 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-#    02111-1307, USA
+#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#    02110-1301, USA
 #
-# 2017-01-10  Julian Wollrath <jwollrath@web.de>
-#
-#     * (0.1) iniial release
-#
+#    2024-10-06 Complete rewrite of module for F::Q issue #414
 
 package Finance::Quote::OnVista;
-require 5.005;
 
 use strict;
-use LWP::UserAgent;
-use HTTP::Request::Common;
+use warnings;
+
+use Encode qw(decode);
 use HTML::TreeBuilder;
-use Encode;
+use HTTP::Request::Common;
+use JSON qw( decode_json );
+
+use constant DEBUG => $ENV{DEBUG};
+use if DEBUG, 'Smart::Comments', '###';
 
 # VERSION
 
-use vars qw($OnVista_URL);
+my $ONVISTA_URL = 'https://www.onvista.de/suche/';
 
-my $OnVista_URL = 'https://www.onvista.de';
+# Change DISPLAY and method values in code below
+# Modify LABELS to those returned by the method
 
-sub methods {return (onvista => \&onvista);}
-sub labels {return ( onvista => [qw/name last date isodate time currency method exchange/] );}
+our $DISPLAY    = 'OnVista - Germany';
+our @LABELS     = qw/symbol name open high low last date volume currency method/;
+our $METHODHASH = {subroutine => \&onvista, 
+                   display => $DISPLAY, 
+                   labels => \@LABELS};
+
+sub methodinfo {
+    return ( 
+        onvista => $METHODHASH,
+    );
+}
+
+sub labels { my %m = methodinfo(); return map {$_ => [@{$m{$_}{labels}}] } keys %m; }
+
+sub methods {
+  my %m = methodinfo(); return map {$_ => $m{$_}{subroutine} } keys %m;
+}
 
 sub onvista {
-    my $quoter = shift;
-    my @stocks = @_;
-    my (%info, $reply);
-    my $ua = $quoter->user_agent();
 
-    foreach my $stock (@stocks) {
-        my $queryUrl = "$OnVista_URL/suche/$stock";
-        $reply = $ua->request(GET $queryUrl);
+  my $quoter = shift;
+  my @stocks = @_;
+  my (%info, $tree, $url, $reply);
+  my $ua = $quoter->user_agent();
 
-        eval {
-            my $tree    = HTML::TreeBuilder->new_from_content(decode_utf8 $reply->content);
-            my $row     = $tree->look_down('_tag' => 'tr', 'class' => 'HERVORGEHOBEN');
-            my $link    = $row->look_down('_tag' => 'a');
-            $queryUrl   = $OnVista_URL . $link->attr('href');
-            $reply      = $ua->request(GET $queryUrl);
-        };
+  foreach my $stock (@stocks) {
 
-        if ($@) {
-            $info{ $stock, "success" }  = 0;
-            $info{ $stock, "errormsg" } = "Error retreiving $stock redirect: $@";
+    $url   = $ONVISTA_URL . $stock;
+    $reply = $ua->request( GET $url);
+
+    my $code    = $reply->code;
+    my $desc    = HTTP::Status::status_message($code);
+    my $headers = $reply->headers_as_string;
+    my $body    = decode('UTF-8', $reply->content);
+
+    ### Body: $body
+
+    my ($name, $bid, $ask, $last, $open, $high, $low, $date);
+
+    $info{ $stock, "symbol" } = $stock;
+
+    if ( $code == 200 ) {
+
+      # Use HTML::TreeBuilder to parse HTML in $body
+      $tree = HTML::TreeBuilder->new;
+      if ($tree->parse($body)) {
+
+        $tree->eof;
+        if ( $tree->look_down(_tag => 'div', id => 'ctl00_body_divNoData') ) {
+          $info{ $stock, "success" } = 0;
+          $info{ $stock, "errormsg" } =
+            "Error retrieving quote for $stock. No data returned";
+          next;
+        }
+        $name = $tree->look_down(_tag => 'h2', class => qr/^mBot0 large textStyled/)->as_text;
+        $info{ $stock, 'success' } = 1;
+        ($info{ $stock, 'name' } = $name) =~ s/^\s+|\s+$//g ;
+        $info{ $stock, 'currency' } = 'RON';
+        $info{ $stock, 'method' } = 'bvb';
+        $table = $tree->look_down(_tag => 'table', id => qr/^ctl00_body_ctl02_PricesControl_dvCPrices/)->as_HTML;
+        $pricetable = HTML::TableExtract->new();
+        $pricetable->parse($table);
+        foreach my $row ($pricetable->rows) {
+          if ( @$row[0] =~ m/Ask$/ ) {
+            ($bid, $ask) = @$row[1] =~ m|^\s+([\d\.]+)\s+\/\s+([\d\.]+)|;
+            $info{ $stock, 'bid' } = $bid;
+            $info{ $stock, 'ask' } = $ask;
+          }
+          elsif ( @$row[0] =~ m|^Date/time| ) {
+            ($date) = @$row[1] =~ m|^([\d/]+)\s|;
+            $quoter->store_date(\%info, $stock, {usdate => $1}) if $date =~ m|([0-9]{1,2}/[0-9]{2}/[0-9]{4})|;
+          }
+          elsif ( @$row[0] =~ m|^Last price| ) {
+            ($last) = @$row[1] =~ m|^([\d\.]+)|;
+            $info{ $stock, 'last' } = $last;
+          }
+          elsif ( @$row[0] =~ m|^Open price| ) {
+            ($open) = @$row[1] =~ m|^([\d\.]+)|;
+            $info{ $stock, 'open' } = $open;
+          }
+          elsif ( @$row[0] =~ m|^High price| ) {
+            ($high) = @$row[1] =~ m|^([\d\.]+)|;
+            $info{ $stock, 'high' } = $high;
+          }
+          elsif ( @$row[0] =~ m|^Low price| ) {
+            ($low) = @$row[1] =~ m|^([\d\.]+)|;
+            $info{ $stock, 'low' } = $low;
+          }
         }
 
-        if ($reply->is_success) {
+      } else {
+        $tree->eof;
+        $info{ $stock, "success" } = 0;
+        $info{ $stock, "errormsg" } =
+          "Error retrieving quote for $stock. Could not parse HTML returned from $url.";
+      }
 
-            $info{ $stock, "success" } = 1;
-
-            my $tree = HTML::TreeBuilder->new_from_content(decode_utf8 $reply->content);
-
-            my @nameline = $tree->look_down('property', 'schema:name');
-
-            if (not @nameline) {
-                @nameline = $tree->look_down('class', 'inline name');
-		if (not @nameline) {
-                    @nameline = $tree->look_down('class', 'ui medium header');
-                }
-            }
-
-            unless (@nameline) {
-                $info{ $stock, "success" } = 0;
-                $info{ $stock, "errormsg" } = "Stock name $stock not retrievable";
-                next;
-            }
-
-            my $name = $nameline[0]->as_text;
-            $name =~ s/^\s+|\s+$//g;
-            $info{ $stock, "name" } = $name;
-
-            $info{ $stock, "method" } = "onvista";
-
-            my $exchange = $tree->look_down('property', 'schema:seller');
-            if ($exchange) {
-                $info{ $stock, "exchange" } = $exchange->attr_get_i('content');
-            }
-
-            my @currencyline = $tree->look_down('property', 'schema:priceCurrency');
-            if (not @currencyline) {
-                @currencyline = $tree->look_down('id', 'current-quote-currency');
-            }
-	    if (@currencyline) {
-                my $currency = $currencyline[0]->as_text;
-                $info{ $stock, "currency" } = $currency;
-            } else {
-                @currencyline = $tree->look_down('class', 'price');
-                my $currency = ( $currencyline[0]->content_list )[0];
-		$currency =~ s/.* //;
-                $info{ $stock, "currency" } = $currency;
-            }
-
-            my @lastline = $tree->look_down('data-push', qr/.*last.*Stock/);
-            if (not @lastline) {
-                @lastline = $tree->look_down('id', 'current-quote-price');
-            }
-            if (not @lastline) {
-                @lastline = $tree->look_down('class', 'price');
-		if (@lastline) {
-                    my $last = ( $lastline[0]->content_list )[0];
-                    $last =~ s/,/./;
-                    $last =~ s/ .*//;
-                    $info{ $stock, "last" } = $last;
-	        } else {
-		    my $last = $tree->look_down('property', 'schema:price');
-		    if ($last) {
-			$info{ $stock, "last" } = $last->attr_get_i('content');
-		    }
-		}
-            } else {
-                my $last = $lastline[0]->as_text;
-                $last =~ s/,/./;
-                $info{ $stock, "last" } = $last;
-            }
-
-            my @datetimeline = $tree->find_by_tag_name('cite');
-            if (not @datetimeline) {
-                @datetimeline = $tree->look_down('id', 'current-quote-datetime');
-		if (@datetimeline) {
-                    my $datetime = $datetimeline[0]->as_text;
-                    my $date = substr($datetime, 4, 2)."/".substr($datetime, 1, 2)."/".substr($datetime, 7, 4);
-                    my $time = substr($datetime, 13, 8);
-                    $info{ $stock, "date" } = $date;
-                    $info{ $stock, "time" } = $time;
-                } else {
-                    @datetimeline = $tree->look_down('class', 'datetime');
-                    my $datetime = ( $datetimeline[0]->content_list )[0];
-                    my $date = substr($datetime, 3, 2)."/".substr($datetime, 0, 2)."/".substr($datetime, 6, 4);
-                    my $time = substr($datetime, 12, 8);
-                    $info{ $stock, "date" } = $date;
-                    $info{ $stock, "time" } = $time;
-                }
-            } else {
-                my $datetime = $datetimeline[0]->as_text;
-                my $date = substr($datetime, 5, 2)."/".substr($datetime, 2, 2)."/".substr($datetime, 8, 4);
-                my $time = substr($datetime, 14, 8);
-                $info{ $stock, "date" } = $date;
-                $info{ $stock, "time" } = $time;
-            }
-
-            $quoter->store_date(\%info, $stock, {usdate => $info{$stock, "date"}});
-            $tree->delete;
-        }
-        else {
-            $info{ $stock, "success" }  = 0;
-            $info{ $stock, "errormsg" } = "Error retreiving $stock";
-        }
+    } else {       # HTTP Request failed (code != 200)
+      $info{ $stock, "success" } = 0;
+      $info{ $stock, "errormsg" } =
+        "Error retrieving quote for $stock. Attempt to fetch the URL $url resulted in HTTP response $code ($desc)";
     }
-    return wantarray() ? %info : \%info;
-    return \%info;
+
+  }  
+
+  return wantarray() ? %info : \%info;
+  return \%info;
+
 }
+
 1;
+
+__END__
 
 =head1 NAME
 
-Finance::Quote::OnVista - Obtain quotes from OnVista.
+Finance::Quote::OnVista - Obtain quotes from Frankfurt Stock Exchange.
 
 =head1 SYNOPSIS
 
@@ -189,26 +164,46 @@ Finance::Quote::OnVista - Obtain quotes from OnVista.
 
     $q = Finance::Quote->new;
 
-    %info = Finance::Quote->fetch("onvista","ml");  # Only query OnVista
+    %info = $q->fetch("onvista", "sap");  # Only query onvista
 
 =head1 DESCRIPTION
 
-This module fetches information from OnVista, https://www.onvista.de. All stocks are available.
+This module fetches information from L<https://onvista.de/>.
 
-This module is loaded by default on a Finance::Quote object. It's
-also possible to load it explicitly by placing "onvista" in the argument
-list to Finance::Quote->new().
+This module is loaded by default on a Finance::Quote object. It's also possible
+to load it explicitly by placing "onvista" in the argument list to
+Finance::Quote->new().
 
-Information obtained by this module may be covered by www.onvista.de
-terms and conditions See https://www.onvista.de/ for details.
+This module provides "onvista" fetch methods. It was written
+to replace a non-working Tradeville.pm module.
+
+Information obtained by this module may be covered by Frankfurt Stock
+Exchange terms and conditions.
 
 =head1 LABELS RETURNED
 
-The following labels may be returned by Finance::Quote::OnVista :
-name, last, date, isodate, time, currency, method, exchange.
+The following labels are returned: 
 
-=head1 SEE ALSO
+=over
 
-OnVista (german web site), https://www.onvista.de
+=item name
 
-=cut
+=item symbol
+
+=item open
+
+=item high
+
+=item low
+
+=item price
+
+=item bid
+
+=item ask
+
+=item date
+
+=item currency
+
+=back
