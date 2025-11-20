876a704cd416026fe7596efeff481680555cf283
Bruce Schuck <bschuck@asgard-systems.com>
2025-11-19 17:21:41 -0800

    Added more headers to User Agent in MarketWatch.pm

--- lib/Finance/Quote/MarketWatch.pm
+++ lib/Finance/Quote/MarketWatch.pm
@@ -68,12 +68,22 @@ sub marketwatch {
   my (%info, $tree, $url, $reply);
   my $ua = $quoter->user_agent();
 
+  $ua->agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+
   my @ua_headers = (
-    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding' => 'gzip, deflate, sdch',
-    'Accept-Language' => 'en-US,en;q=0.8',
+    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language' => 'en-US,en;q=0.9',
+    'Accept-Encoding' => 'gzip, deflate, br',
+    'Connection' => 'keep-alive',
+    'Upgrade-Insecure-Requests' => '1',
+    'Sec-Fetch-Dest' => 'document',
+    'Sec-Fetch-Mode' => 'navigate',
+    'Sec-Fetch-Site' => 'none',
+    'Sec-Fetch-User' => '?1',
   );
+
+  # Enable cookie handling (websites often check if you can store cookies)
+  $ua->cookie_jar({});
 
   foreach my $stock (@stocks) {
 
